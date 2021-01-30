1a5ed55f2cae863b784242707ea1090edc4b2ed3
chetashastry <78254121+chetashastry@users.noreply.github.com>
2021-01-30 17:08:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 Name_likit: <input type="text" name="name"><br>
 E-mail-email: <input type="manju" name="email"><br>
 Password: <input type="punith" name="email"><br>
-Mobile_number: <input type="chetan" name="email"><br>
+Mobile_number: 9663781904 type="chetan" name="email"><br>
 jay: <input type="text" name="email"><br>
-madhu: <input type="text" name="email"><br>
+madhu123: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
